c784634013d1e1ea5c985039792c099e1182ab62
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2024-03-08 21:21:39 +1300

    Added custom cows to project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # FooBar
 
-An example project for the CS399 Workshop that has cool cowsay features!
+An example project for the CS399 Workshop that has cool custom cowsay features!
--- src/foobar/__init__.py
+++ src/foobar/__init__.py
@@ -3,5 +3,5 @@ from cowsay import cowsay
 __all__ = ["say_message"]
 
 
-def say_message(message: str) -> None:
-    print(cowsay(message))
+def say_message(message: str, cow: str = "default") -> None:
+    print(cowsay(message, cow=cow))
